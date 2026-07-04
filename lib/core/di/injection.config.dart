773c56fffa56a7a192e1306a3dd13aa6b097a00f
James Angelo Sarona <jacsarona.official@gmail.com>
2026-07-05 00:47:52 +0800

    fix: build error

--- lib/core/di/injection.config.dart
+++ lib/core/di/injection.config.dart
@@ -1,4 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// dart format width=80
 
 // **************************************************************************
 // InjectableConfigGenerator
@@ -8,57 +9,64 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:bitetrack/core/network/dio_client.dart' as _i5;
-import 'package:bitetrack/core/storage/token_storage.dart' as _i3;
+import 'package:bitetrack/core/network/dio_client.dart' as _i865;
+import 'package:bitetrack/core/storage/token_storage.dart' as _i311;
 import 'package:bitetrack/features/auth/data/datasources/auth_remote_datasource.dart'
-    as _i6;
+    as _i806;
 import 'package:bitetrack/features/auth/data/repositories/auth_repository_impl.dart'
-    as _i8;
+    as _i920;
 import 'package:bitetrack/features/auth/domain/repositories/auth_repository.dart'
-    as _i7;
+    as _i642;
 import 'package:bitetrack/features/auth/domain/usecases/auth_usecases.dart'
-    as _i9;
+    as _i108;
 import 'package:bitetrack/features/auth/presentation/bloc/auth_bloc.dart'
-    as _i10;
-import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart' as _i2;
-import 'package:shared_preferences/shared_preferences.dart' as _i4;
+    as _i1041;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+import 'package:shared_preferences/shared_preferences.dart' as _i460;
 
-extension GetItInjectableX on _i1.GetIt {
-// initializes the registration of main-scope dependencies inside of GetIt
-  _i1.GetIt init({
+extension GetItInjectableX on _i174.GetIt {
+  // initializes the registration of main-scope dependencies inside of GetIt
+  _i174.GetIt init({
     String? environment,
-    _i2.EnvironmentFilter? environmentFilter,
+    _i526.EnvironmentFilter? environmentFilter,
   }) {
-    final gh = _i2.GetItHelper(
-      this,
-      environment,
-      environmentFilter,
+    final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    gh.lazySingleton<_i311.TokenStorage>(
+      () => _i311.TokenStorage(gh<_i460.SharedPreferences>()),
     );
-    gh.lazySingleton<_i3.TokenStorage>(
-        () => _i3.TokenStorage(gh<_i4.SharedPreferences>()));
-    gh.lazySingleton<_i5.DioClient>(
-        () => _i5.DioClient(gh<_i3.TokenStorage>()));
-    gh.lazySingleton<_i6.AuthRemoteDataSource>(
-        () => _i6.AuthRemoteDataSource(gh<_i5.DioClient>()));
-    gh.lazySingleton<_i7.AuthRepository>(() => _i8.AuthRepositoryImpl(
-          gh<_i6.AuthRemoteDataSource>(),
-          gh<_i3.TokenStorage>(),
-        ));
-    gh.factory<_i9.GetCurrentUserUseCase>(
-        () => _i9.GetCurrentUserUseCase(gh<_i7.AuthRepository>()));
-    gh.factory<_i9.LoginUseCase>(
-        () => _i9.LoginUseCase(gh<_i7.AuthRepository>()));
-    gh.factory<_i9.LogoutUseCase>(
-        () => _i9.LogoutUseCase(gh<_i7.AuthRepository>()));
-    gh.factory<_i9.RegisterUseCase>(
-        () => _i9.RegisterUseCase(gh<_i7.AuthRepository>()));
-    gh.factory<_i10.AuthBloc>(() => _i10.AuthBloc(
-          getCurrentUser: gh<_i9.GetCurrentUserUseCase>(),
-          login: gh<_i9.LoginUseCase>(),
-          register: gh<_i9.RegisterUseCase>(),
-          logout: gh<_i9.LogoutUseCase>(),
-        ));
+    gh.lazySingleton<_i865.DioClient>(
+      () => _i865.DioClient(gh<_i311.TokenStorage>()),
+    );
+    gh.lazySingleton<_i806.AuthRemoteDataSource>(
+      () => _i806.AuthRemoteDataSource(gh<_i865.DioClient>()),
+    );
+    gh.lazySingleton<_i642.AuthRepository>(
+      () => _i920.AuthRepositoryImpl(
+        gh<_i806.AuthRemoteDataSource>(),
+        gh<_i311.TokenStorage>(),
+      ),
+    );
+    gh.factory<_i108.LoginUseCase>(
+      () => _i108.LoginUseCase(gh<_i642.AuthRepository>()),
+    );
+    gh.factory<_i108.RegisterUseCase>(
+      () => _i108.RegisterUseCase(gh<_i642.AuthRepository>()),
+    );
+    gh.factory<_i108.GetCurrentUserUseCase>(
+      () => _i108.GetCurrentUserUseCase(gh<_i642.AuthRepository>()),
+    );
+    gh.factory<_i108.LogoutUseCase>(
+      () => _i108.LogoutUseCase(gh<_i642.AuthRepository>()),
+    );
+    gh.factory<_i1041.AuthBloc>(
+      () => _i1041.AuthBloc(
+        getCurrentUser: gh<_i108.GetCurrentUserUseCase>(),
+        login: gh<_i108.LoginUseCase>(),
+        register: gh<_i108.RegisterUseCase>(),
+        logout: gh<_i108.LogoutUseCase>(),
+      ),
+    );
     return this;
   }
 }
